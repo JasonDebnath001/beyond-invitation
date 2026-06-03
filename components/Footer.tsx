27a4fdf39d67aee420b1ab784935ac114585c428
unknown <aritradebnath1@gmail.com>
2026-06-03 14:35:34 +0530

    links change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ const footerColumns = [
 const socials = [
   {
     label: "Instagram",
-    href: "https://www.instagram.com",
+    href: "https://www.instagram.com/beyond_invitationofficial/",
     icon: (
       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
         <rect x="3.5" y="3.5" width="17" height="17" rx="5" />
@@ -38,7 +38,7 @@ const socials = [
   },
   {
     label: "Facebook",
-    href: "https://www.facebook.com",
+    href: "https://www.facebook.com/weddingcardsmanufacturer2022",
     icon: (
       <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
         <path d="M13.5 21v-8h2.7l.4-3.1h-3.1V7.9c0-.9.25-1.5 1.55-1.5H17V3.6c-.3-.04-1.3-.13-2.5-.13-2.5 0-4.2 1.5-4.2 4.3v2.4H7.6V13h2.7v8h3.2Z" />
@@ -47,19 +47,10 @@ const socials = [
   },
   {
     label: "YouTube",
-    href: "https://www.youtube.com",
+    href: "https://www.youtube.com/@BeyondInvitation",
     icon: (
       <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
         <path d="M22 8.2a3 3 0 0 0-2.1-2.1C18 5.5 12 5.5 12 5.5s-6 0-7.9.6A3 3 0 0 0 2 8.2 31 31 0 0 0 1.7 12 31 31 0 0 0 2 15.8a3 3 0 0 0 2.1 2.1c1.9.6 7.9.6 7.9.6s6 0 7.9-.6a3 3 0 0 0 2.1-2.1c.3-1.9.3-3.8.3-3.8s0-1.9-.3-3.8ZM10 15V9l5.2 3-5.2 3Z" />
-      </svg>
-    ),
-  },
-  {
-    label: "Pinterest",
-    href: "https://www.pinterest.com",
-    icon: (
-      <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M12 2a10 10 0 0 0-3.6 19.3c-.1-.8-.1-2 .1-2.9l1.2-5s-.3-.6-.3-1.5c0-1.4.8-2.4 1.8-2.4.9 0 1.3.6 1.3 1.4 0 .9-.55 2.2-.85 3.4-.25 1 .5 1.85 1.5 1.85 1.8 0 3.1-1.9 3.1-4.6 0-2.4-1.7-4.1-4.2-4.1-2.9 0-4.6 2.15-4.6 4.4 0 .9.35 1.8.8 2.3a.3.3 0 0 1 .07.3l-.27 1.1c-.05.2-.16.25-.36.15-1.3-.6-2.1-2.5-2.1-4 0-3.25 2.35-6.25 6.8-6.25 3.55 0 6.3 2.55 6.3 5.95 0 3.55-2.2 6.4-5.3 6.4-1.05 0-2.03-.55-2.37-1.2l-.65 2.45c-.23.9-.85 2-1.27 2.7A10 10 0 1 0 12 2Z" />
       </svg>
     ),
   },
